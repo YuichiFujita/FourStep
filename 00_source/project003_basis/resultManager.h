2dd694dadd2c53aefad9e0c53a94242db7827bce
藤田勇一 <221065@st.yoshida-g.ac.jp>
2023-12-25 18:40:19 +0900

    リトライ性の向上

--- 00_source/project003_basis/resultManager.h
+++ 00_source/project003_basis/resultManager.h
@@ -27,9 +27,6 @@ public:
 	enum ETexture
 	{
 		TEXTURE_SCORE = 0,	// スコアテクスチャ
-		TEXTURE_CONTINUE,	// コンテニュー表示テクスチャ
-		TEXTURE_YES,		// YESテクスチャ
-		TEXTURE_NO,			// NOテクスチャ
 		TEXTURE_MAX			// この列挙型の総数
 	};
 
@@ -39,18 +36,8 @@ public:
 		STATE_NONE = 0,			// 何もしない状態
 		STATE_FADEIN,			// フェードイン状態
 		STATE_RESULT,			// スコア表示状態
-		STATE_CONTINUE_WAIT,	// コンテニュー表示待機状態
-		STATE_CONTINUE,			// コンテニュー表示状態
 		STATE_WAIT,				// 遷移待機状態
 		STATE_MAX				// この列挙型の総数
-	};
-
-	// 選択列挙
-	enum ESelect
-	{
-		SELECT_YES = 0,	// YES
-		SELECT_NO,		// NO
-		SELECT_MAX		// この列挙型の総数
 	};
 
 	// コンストラクタ
@@ -72,8 +59,6 @@ private:
 	// メンバ関数
 	void UpdateFade(void);			// フェードイン
 	void UpdateResult(void);		// リザルト表示
-	void UpdateContinue(void);		// コンテニュー表示
-	void UpdateSelect(void);		// 選択更新
 	void UpdateTransition(void);	// 遷移決定
 	void SkipStaging(void);			// 演出スキップ
 	bool UpdateDrawWait(const int nWait);	// 表示待機
@@ -82,15 +67,11 @@ private:
 	static const char *mc_apTextureFile[];	// テクスチャ定数
 
 	// メンバ変数
-	CObject2D *m_apContinue[SELECT_MAX];	// コンテニュー表示の情報
 	CObject2D *m_pScoreTitle;	// スコアタイトルの情報
 	CMultiValue *m_pScore;		// スコアの情報
-	CObject2D *m_pContLogo;		// コンテニューロゴの情報
 	CObject2D *m_pFade;			// フェードの情報
 	EState m_state;				// 状態
 	int m_nCounterState;		// 状態管理カウンター
-	int m_nSelect;				// 現在の選択
-	int m_nOldSelect;			// 前回の選択
 	float m_fScale;				// ポリゴン拡大率
 };
 
